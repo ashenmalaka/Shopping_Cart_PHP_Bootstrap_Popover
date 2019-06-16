d4583428ce66debac63df9d2579e82042dd346c3
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-16 08:48:32 +0530

    Added js and bootstrap libraries

--- index.php
+++ index.php
@@ -6,9 +6,23 @@
 <html>
     <head>
         <title>Shopping Cart - PHP & Bootstrap Popover</title>
+        <script src="javascript/jquery.min.js"></script>
+        <link href="stylesheet" href="css/bootstrap.min.css">
+        <script src="javascript/bootstrap.min.js"></script>
+        <style>
+            .popover
+            {
+                width: 100%;
+                max-width: 800px;
+            }
+        </style>
     </head>
     <body>
-    
+
+
+
+
+
     </body>
 </html>
 
