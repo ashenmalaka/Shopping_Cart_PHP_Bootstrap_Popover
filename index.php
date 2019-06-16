1fa810996776e651f091f952d54d4158478a213c
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-16 09:18:50 +0530

    Added codes for shopping cart navigational bar

--- index.php
+++ index.php
@@ -19,6 +19,53 @@
     </head>
     <body>
 
+    <div class="container">
+        <br />
+        <h3 align="center"><a href="#">Shopping Cart by using Bootstrap Popover & PHP</a></h3>
+        <br />
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Menu</span>
+                        <span class="glyphicon glyphicon-menu-hamburger"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">Shopping Cart</a>
+                </div>
+
+                <div id="navbar-cart" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a id="cart-popover" class="btn" data-placement="bottom" title="Shopping Cart">
+                                <span class="glyphicon glyphicon-shopping-cart"></span>
+                                <span class="badge"></span>
+                                <span class="total_price">$ 0.00</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+
+        <div id="popover_content_wrapper" style="display: none">
+            <span id="cart_details"></span>
+            <div align="right">
+                <a href="#" class="btn btn-primary" id="check_out_cart">
+                    <span class="glyphicon glyphicon-shopping-cart"></span> Check out
+                </a>
+                <a href="#" class="btn btn-default" id="clear_cart">
+                    <span class="glyphicon glyphicon-trash"></span> Clear
+                </a>
+            </div>
+        </div>
+
+        <div id="display_item">
+
+
+        </div>
+
+    </div>
 
 
 
